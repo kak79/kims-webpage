75ad5a55d05692fc7160341f96b93d51d496b8a5
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-07-18 15:59:41 -0500

    added key

--- src/component/blog_container/blogs.js
+++ src/component/blog_container/blogs.js
@@ -5,10 +5,10 @@ const Blogs = (props) => {
     <>
       {props.blogs.map((blog) => {
         return (
-          <>
+          <div key={blog.id}>
             <h2>{blog.title}</h2>
             <h4>{blog.summary}</h4>
-          </>
+          </div>
         )
       })}
     </>
